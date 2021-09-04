f9a79e5f1151afcefc787e541e21746494fa7295
Mohammad Bilal <mdbilal7620@gmail.com>
2021-09-04 20:53:21 +0530

    Navigation Routers

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,23 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:recipo/category_meals_screen.dart';
 
 class CategoriItem extends StatelessWidget {
   final String title;
   final Color color;
   CategoriItem(this.title, this.color);
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoryMealScreen();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.title,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [color.withOpacity(0.7), color],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(15)),
       ),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [color.withOpacity(0.7), color],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight),
-          borderRadius: BorderRadius.circular(15)),
     );
   }
 }
