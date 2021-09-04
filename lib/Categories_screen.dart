34bfd19f16b7ec66d8eab1265f340a7def76681f
Mohammad Bilal <mdbilal7620@gmail.com>
2021-09-04 19:47:05 +0530

    Categories added

--- lib/Categories_screen.dart
+++ lib/Categories_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:recipo/category_item.dart';
+import 'package:recipo/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-        children: [],
+        children:
+            DUMMY_DATA.map((e) => CategoriItem(e.title, e.color)).toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200,
             childAspectRatio: 1.5,
--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -1,6 +1,11 @@
-import 'lib\models\category.dart';
+import './models/category.dart';
 import 'package:flutter/material.dart';
 
 const DUMMY_DATA = const [
-  Category("d", "d", Colors.amber),
+  Category("c1", "Itaian", Colors.blue),
+  Category("c2", "Indian", Colors.blue),
+  Category("c3", "Arabic", Colors.blue),
+  Category("c4", "Mexican", Colors.blue),
+  Category("c5", "Fast Food", Colors.blue),
+  Category("c6", "Chicken", Colors.blue),
 ];
