49dc79624c38bd7ab66c4b98730756090ba39f52
Mohammad Bilal <mdbilal7620@gmail.com>
2021-09-04 20:28:15 +0530

    Fonts applied

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -8,4 +8,10 @@ const DUMMY_DATA = const [
   Category("c4", "Mexican", Colors.orangeAccent),
   Category("c5", "Fast Food", Colors.red),
   Category("c6", "Chicken", Colors.yellow),
+  Category("c7", "Itaian", Colors.lightGreen),
+  Category("c8", "Indian", Colors.blue),
+  Category("c9", "Arabic", Colors.indigo),
+  Category("c10", "Mexican", Colors.orangeAccent),
+  Category("c11", "Fast Food", Colors.red),
+  Category("c12", "Chicken", Colors.yellow),
 ];
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Recipo",
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Railway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              title: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'RobotoCondensed'))),
       home: CategoriesScreen(),
     );
   }
