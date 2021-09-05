ff9db4940113683683784542a5ce838c01c14f1e
Mohammad Bilal <mdbilal7620@gmail.com>
2021-09-05 13:44:16 +0530

    Null Safety REmoved'

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -10,7 +10,7 @@ class CategoryMealScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final routeArgs =
-        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryId = routeArgs['id'];
     final categoryTitle = routeArgs['title'];
     final categoryMeals = MEALS_DATA.where((meal) {
--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -4,20 +4,20 @@ enum Complexity { Simple, Medium, Hard }
 enum Affordability { Affordable, Pricey, Luxurious }
 
 class Meal {
-  final String? id;
-  final List<String>? categories;
-  final String? title;
-  final String? imageUrl;
-  final List<String>? ingredients;
-  final List<String>? steps;
-  final int? duration;
-  final Complexity? complexity;
-  final Affordability? affordability;
+  final String id;
+  final List<String> categories;
+  final String title;
+  final String imageUrl;
+  final List<String> ingredients;
+  final List<String> steps;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
 
-  final bool? isGlutenFree;
-  final bool? isLactoseFree;
-  final bool? isVegan;
-  final bool? isVegeterian;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegeterian;
 
   const Meal({
     @required this.id,
