d6e2298efac30dd26306684f0533891f8b110542
Mohammad Bilal <mdbilal7620@gmail.com>
2021-09-08 21:01:56 +0530

    Drawer added

--- lib/screens/tab_screen.dart
+++ lib/screens/tab_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recipo/screens/Categories_screen.dart';
 import 'package:recipo/screens/favourite_screen.dart';
+import 'package:recipo/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -8,25 +9,43 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  List<Widget> _pages = [
+    CategoriesScreen(),
+    FavouriteScreen(),
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        // initialIndex: 1,
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Recipo"),
-            bottom: TabBar(
-              automaticIndicatorColorAdjustment: true,
-              tabs: [
-                Tab(icon: Icon(Icons.category), text: 'Categories'),
-                Tab(icon: Icon(Icons.favorite), text: 'Favourites'),
-              ],
-            ),
-          ),
-          body: TabBarView(
-            children: [CategoriesScreen(), FavouriteScreen()],
-          ),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Recipo"),
+        ),
+        body: _pages[_selectedPageIndex],
+        drawer: MainDrawer(),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedPageIndex,
+          backgroundColor: Colors.pink,
+          unselectedItemColor: Colors.black54,
+          selectedItemColor: Colors.white,
+          type: BottomNavigationBarType.shifting,
+          onTap: _selectPage,
+          items: [
+            BottomNavigationBarItem(
+                backgroundColor: Colors.pink,
+                icon: Icon(Icons.category),
+                title: Text("Categories")),
+            BottomNavigationBarItem(
+                backgroundColor: Colors.pink,
+                icon: Icon(Icons.favorite),
+                title: Text("Favourites")),
+          ],
         ));
   }
 }
